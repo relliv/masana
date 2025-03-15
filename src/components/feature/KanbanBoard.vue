9293a6286fa795cfb8efd270c2cb535754e498e5
Eyüp <epsilonelittr@gmail.com>
2025-03-15 18:17:15 +0300

    Add drawer component

--- src/components/feature/KanbanBoard.vue
+++ src/components/feature/KanbanBoard.vue
@@ -1,74 +1,121 @@
 <template>
-  <div class="kanban-container custom-scrollbar-container">
-    <Container
-      orientation="horizontal"
-      @drop="onColumnDrop($event)"
-      drag-handle-selector=".column-drag-handle"
-      @drag-start="dragStart"
-      :drop-placeholder="upperDropPlaceholderOptions"
-      class="kanban custom-scrollbar"
-    >
-      <Draggable
-        v-for="column in scene.columns"
-        :key="column.id"
-        class="column custom-scrollbar-container"
+  <div data-vaul-drawer-wrapper="">
+    <div class="kanban-container custom-scrollbar-container">
+      <Container
+        orientation="horizontal"
+        @drop="onColumnDrop($event)"
+        drag-handle-selector=".column-drag-handle"
+        @drag-start="dragStart"
+        :drop-placeholder="upperDropPlaceholderOptions"
+        class="kanban custom-scrollbar"
       >
-        <div class="header">
-          <div class="left">
-            <span class="title">
-              {{ column.name }}
-            </span>
-
-            <span class="count">
-              {{ column.tasks.length }}
-            </span>
-          </div>
-
-          <div class="right">
-            <!-- Add New Task -->
-            <button @click="addNewTask(column.id)">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-plus"
-              >
-                <path d="M5 12h14" />
-                <path d="M12 5v14" />
-              </svg>
-            </button>
-          </div>
-        </div>
-
-        <Container
-          group-name="col"
-          @drop="(e) => onCardDrop(column.id, e)"
-          @drag-start="(e) => log('drag start', e)"
-          @drag-end="(e) => log('drag end', e)"
-          :get-child-payload="getCardPayload(column.id)"
-          drag-class="task-ghost"
-          drop-class="card-ghost-drop"
-          :drop-placeholder="dropPlaceholderOptions"
-          class="task-list custom-scrollbar"
+        <Draggable
+          v-for="column in scene.columns"
+          :key="column.id"
+          class="column custom-scrollbar-container"
         >
-          <Draggable v-for="card in column.tasks" :key="card.id">
-            <div class="task">
-              <p>{{ card.title }}</p>
+          <div class="header">
+            <div class="left">
+              <span class="title">
+                {{ column.name }}
+              </span>
+
+              <span class="count">
+                {{ column.tasks.length }}
+              </span>
             </div>
-          </Draggable>
-        </Container>
-      </Draggable>
-    </Container>
+
+            <div class="right">
+              <!-- Add New Task -->
+              <button @click="addNewTask(column.id)">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-plus"
+                >
+                  <path d="M5 12h14" />
+                  <path d="M12 5v14" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <Container
+            group-name="col"
+            @drop="(e) => onCardDrop(column.id, e)"
+            @drag-start="(e) => log('drag start', e)"
+            @drag-end="(e) => log('drag end', e)"
+            :get-child-payload="getCardPayload(column.id)"
+            drag-class="task-ghost"
+            drop-class="card-ghost-drop"
+            :drop-placeholder="dropPlaceholderOptions"
+            class="task-list custom-scrollbar"
+          >
+            <Draggable v-for="card in column.tasks" :key="card.id">
+              <button class="task" @click="onTaskClick(card)">
+                <p>{{ card.title }}</p>
+              </button>
+            </Draggable>
+          </Container>
+        </Draggable>
+      </Container>
+    </div>
+
+    <DrawerRoot v-model:open="isTaskDetailsDrawerOpen">
+      <DrawerTrigger as-child>
+        <button data-testid="trigger" class="text-2xl">Open Drawer</button>
+      </DrawerTrigger>
+      <DrawerPortal>
+        <DrawerOverlay
+          data-testid="overlay"
+          class="fixed inset-0 bg-black/40"
+        />
+        <DrawerContent
+          data-testid="content"
+          class="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0"
+        >
+          <div class="p-4 bg-white rounded-t-[10px] flex-1">
+            <div
+              class="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8"
+            />
+            <div class="max-w-md mx-auto">
+              <DrawerTitle class="font-medium mb-4">
+                Unstyled drawer for Vue.
+              </DrawerTitle>
+            </div>
+            <p class="text-zinc-600 mb-2">
+              This component can be used as a replacement for a Dialog on mobile
+              and tablet devices.
+            </p>
+          </div>
+          <button
+            type="button"
+            data-testid="dismiss-button"
+            class="rounded-md mb-6 w-full bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            @click="() => (open = false)"
+          >
+            Click to close
+          </button>
+        </DrawerContent>
+      </DrawerPortal>
+    </DrawerRoot>
   </div>
 </template>
 
 <script>
+import {
+  DrawerContent,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerRoot,
+} from "vaul-vue";
 import { Container, Draggable } from "vue3-smooth-dnd";
 import { applyDrag, generateItems } from "../../shared/utils/array";
 import { v4 as uuidv4 } from "uuid";
@@ -101,7 +148,14 @@ const scene = {
 };
 
 export default {
-  components: { Container, Draggable },
+  components: {
+    Container,
+    Draggable,
+    DrawerContent,
+    DrawerOverlay,
+    DrawerPortal,
+    DrawerRoot,
+  },
 
   data() {
     return {
@@ -116,6 +170,7 @@ export default {
         animationDuration: "150",
         showOnTop: true,
       },
+      isTaskDetailsDrawerOpen: false,
     };
   },
 
@@ -169,6 +224,10 @@ export default {
         });
         this.scene = scene;
       }
+    },
+
+    onTaskClick(task) {
+      this.isTaskDetailsDrawerOpen = true;
     },
   },
 };
